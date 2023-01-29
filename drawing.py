Added FM param interpolation, fixed spectrum drawing scaling problem & app synth update bug

drawing.py
@@ -24,6 +24,7 @@ class DataAnimation(metaclass=ABCMeta):
         self._precision_bits = 6
         self._precision_mult = 2 ** self._precision_bits
         self._bbox_height = self._bbox['bottom'] - self._bbox['top']
+        self._bbox_width = self._bbox['right'] - self._bbox['left']
 
     @abstractmethod
     def set_samples(self, samples):
@@ -75,7 +76,9 @@ class AnimatedSpectrum(DataAnimation):
         spectrum=spectrum - np.min(spectrum)
         spectrum_normalized =1.0- spectrum / np.max(spectrum) # flip y
         y_values = spectrum_normalized *  self._bbox_height + self._bbox['top']
-        x_values = np.linspace(self._bbox['left'], self._bbox['right'] - 1, y_values.size)
+        x_values_unit = (self._power_f[f_valid] - self._f_range[0]) / (self._f_range[1]-self._f_range[0])
+        x_values= x_values_unit * self._bbox_width + self._bbox['left']
+
 
         self._coords = (self._precision_mult * np.dstack([x_values, y_values])).reshape(-1, 1, 2).astype(np.int32)
 

fm_synth.py
@@ -10,33 +10,48 @@ class FMSynthesizer(object):
     """
 
     """
+
     def __init__(self, rate=44100.0, carrier_init=(220.0, 0.5), modulation_init=(1.0, 0.0)):
         self._rate = rate
-        self._carrier_freq, self._carrier_amp = carrier_init
-        self._modulation_freq, self._modulation_depth = modulation_init
+        self._params = {'carrier_freq': carrier_init[0],
+                        'carrier_amp': carrier_init[1],
+                        'mod_freq': modulation_init[0],
+                        'mod_depth': modulation_init[0]}
+
+        self._last_params = {p: None for p in self._params}  # for smooth interpolation
+
         self._t_offset = 0  # keep track of time offset to avoid pops when generating successive buffers
 
     def __str__(self):
-        return "FMSynth(C_f = %.3f Hz, C_a = %.2f %%, M_f = %.3f Hz. M_d = %.2f)" % (self._carrier_freq,
-                                                                                     self._carrier_amp * 100,
-                                                                                     self._modulation_freq,
-                                                                                     self._modulation_depth)
+        return "FMSynth(C_f = %.3f Hz, C_a = %.2f %%, M_f = %.3f Hz. M_d = %.2f)" % (self._params['carrier_freq'],
+                                                                                     self._params['carrier_amp'] * 100,
+                                                                                     self._params['mod_freq'],
+                                                                                     self._params['mod_depth'])
 
-    def set_params(self, c_freq=None, c_amp=None, m_freq=None, m_depth=None):
-        self._carrier_freq = c_freq if c_freq is not None else self._carrier_freq
-        self._carrier_amp = c_amp if c_amp is not None else self._carrier_amp
-        self._modulation_freq = m_freq if m_freq is not None else self._modulation_freq
-        self._modulation_depth = m_depth if m_depth is not None else self._modulation_depth
+    def set_param(self, name, value):
+        """
+        Remember the previous value for smooth interpolation
+        TODO:   Keep ALL previous values, tag w/timestamp, so values changing too fast aren't lost.
+        """
+        self._last_params[name] = self._params[name]
+        self._params[name] = value
 
-    def get_samples(self, n, t_0=0.0):
+    def get_samples(self, n, t_0=0.0, smoothed=False):
         """
         Get the next N samples and the time of sample n+1
         """
         total_time = n / self._rate
         t = np.linspace(0, total_time, n + 1)[1:] + t_0
 
-        modulation = np.sin(np.pi * 2.0 * self._modulation_freq * t) * self._modulation_depth
-        samples = np.sin(np.pi * 2.0 * self._carrier_freq * t + modulation) * self._carrier_amp
+        params = self._params.copy()
+        if smoothed:
+            for name in self._params:
+                if self._last_params[name] is not None:
+                    params[name] = np.linspace(self._last_params[name], self._params[name], n)
+                    self._last_params[name] = None  # only do this once!
+
+        modulation = np.sin(np.pi * 2.0 * params['mod_freq'] * t) * params['mod_depth']
+        samples = np.sin(np.pi * 2.0 * params['carrier_freq'] * t + modulation) * params['carrier_amp']
         return samples, t[-1]
 
     def get_next_samples(self, n_samples):