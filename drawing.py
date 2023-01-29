b594026e72b95e6746a639cb02d7e2bb568bb000
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2023-01-28 18:32:36 -0800

    Added power spectrum, next markers in coordinate grids

--- drawing.py
+++ drawing.py
@@ -1,100 +1,103 @@
 import numpy as np
 import time
 import cv2
-from gui_utils.drawing import draw_rect,draw_box
+from gui_utils.drawing import draw_rect, draw_box
+from scipy.fft import fft  # fastest
+from numpy.fft import fftfreq
+from abc import abstractmethod, ABCMeta
 
-class AnimatedWave(object):
-    def __init__(self, bbox, wave_func, bbox_thickness=2, wave_color=(255, 255, 255, 255)):
+
+class DataAnimation(metaclass=ABCMeta):
+    """
+    Generic live display of data
+    """
+
+    def __init__(self, bbox, color=(255, 255, 255, 255)):
+        """
+        :param bbox: dict with 'top','left','right','bottom', where in image to draw animation
+        """
         self._bbox = bbox
-        self._wave_func = wave_func
-        self._wave_color = wave_color
+        self._color = color
+        self._coords = None
         self._last_params = None
         self._last_coords = None
         self._precision_bits = 6
-        self._bbox_thickness = bbox_thickness
         self._precision_mult = 2 ** self._precision_bits
-        self._x_values = None
-        self._t_start =time.perf_counter()
+        self._bbox_height = self._bbox['bottom'] - self._bbox['top']
 
-    def draw(self, image, params=None):
+    @abstractmethod
+    def set_samples(self, samples):
+        """
+        re-define current frame
+        """
+        pass
+
+    def draw(self, image):
         """
         Render wave in image.  Params optional, used to avoid re-calculating coords.
         """
-        #if time.perf_counter()-self._t_start > 2.0:
-        #    import ipdb;
-        #    ipdb.set_trace()
-        # Draw bounding box:
-        if self._bbox_thickness> 0:
-            draw_box(image, self._bbox,self._bbox_thickness, self._wave_color)
+        cv2.polylines(image, [self._coords], False, self._color, 1, cv2.LINE_AA, self._precision_bits)
 
-        # update coords
-        if self._last_params is None or params is None or self._last_params != params:
-            self._last_coords = self._get_wave_image_coords()
-        self._last_params = params
 
-        # draw
-        cv2.polylines(image, [self._last_coords], False, self._wave_color, 1, cv2.LINE_AA, self._precision_bits)
+class AnimatedSpectrum(DataAnimation):
 
-    def _get_wave_image_coords(self):
+    def __init__(self, bbox, f_range, sampling_rate=44100, color=(255, 255, 255, 255)):
         """
-        How to render a wave in a given bounding box of an image
+        bbox-bounding box
+        f_range (f_low, f_high) for display
+        sampling_rate to get frequencies correct in FFT.
         """
+        super(AnimatedSpectrum, self).__init__(bbox, color=color)
+        self._f_range = f_range
+        self._p_range = (0.0, 1.0)
+        self._sampling_rate = sampling_rate
+        self._samples = None
+        self._p = None
 
-        samples = self._wave_func()
-        y_range = self._bbox['bottom'] - self._bbox['top']
+    def set_f_range(self, f_range, p_range):
+        self._f_range = f_range
+        self._recalculate_coords()
 
-        y_values = (-samples / 2. + 0.5) * y_range + self._bbox['top']
-        if self._x_values is None or self._x_values.size!=samples.size:
-            self._x_values = np.linspace(self._bbox['left'], self._bbox['right'] - 1, samples.size)
+    def set_samples(self, samples):
+        self._samples = samples
+        self._recalculate_spectrum()
+        self._recalculate_coords()
 
-        return (self._precision_mult * np.dstack([self._x_values, y_values])).reshape(-1, 1, 2).astype(np.int32)
+    def _recalculate_coords(self):
+        """
+        Prune spectrum, scale to window
+        """
+        # first determine frequencies
+        f_valid =np.logical_and(self._power_f >= self._f_range[0], self._power_f <= self._f_range[1])
 
+        # apply cutoff and scale
+        spectrum = self._log_power[f_valid]
+        spectrum=spectrum - np.min(spectrum)
+        spectrum_normalized =1.0- spectrum / np.max(spectrum) # flip y
+        y_values = spectrum_normalized *  self._bbox_height + self._bbox['top']
+        x_values = np.linspace(self._bbox['left'], self._bbox['right'] - 1, y_values.size)
 
-def test_wave():
-    slowdown = 1000.0
-    frequency = 220.0
+        self._coords = (self._precision_mult * np.dstack([x_values, y_values])).reshape(-1, 1, 2).astype(np.int32)
 
-    t_start = time.perf_counter()
+    def _recalculate_spectrum(self):
+        """
+        Determine shape of spectrum curve, from samples & Frequency bounds
+        """
+        # now get spectrum
+        size = int(self._samples.size/2)
+        z = fft(self._samples)[:size]
+        self._log_power = np.log10(np.abs(z) ** 2.)
+        f = fftfreq(self._samples.size, 1. / self._sampling_rate)[:size]
 
-    def _get_t_offset():
-        dt = (time.perf_counter() - t_start)
-        if int(dt) % 4 <= 1:
-            dt = 0
-        else:
-            dt /= slowdown
-        return dt
-
-    def _wave_func(n):
-        t = np.linspace(0.0, n / 44100., n + 1)[:-1] + _get_t_offset()
-
-        return np.sin(np.pi * 2. * frequency * t)
-
-    bkg = np.zeros((500, 750, 4), dtype=np.uint8)
-    bkg[:, :, 3] = 255
-    bbox = {'top': 0, 'left': 0, 'right': bkg.shape[1], 'bottom': bkg.shape[0]}
-    wave = AnimatedWave(bbox, _wave_func)
-
-    fps_t_start = time.perf_counter()
-    n_frames = 0
-    draw_times = []
-    while True:
-        frame = bkg.copy()
-        draw_start = time.perf_counter()
-        wave.draw(frame, params=_get_t_offset())
-        draw_times.append(time.perf_counter() - draw_start)
-        cv2.imshow("Wave - q to quit", frame)
-        k = cv2.waitKey(1)
-        if k & 0xff == ord('q'):
-            break
-
-        n_frames += 1
-        elapsed = time.perf_counter() - fps_t_start
-        if elapsed > .5:
-            draw_fps = 1.0 / np.array(draw_times)
-            print("Mean FPS:  %.1f   (sd.  %.3f)" % (float(np.mean(draw_fps)), float(np.std(draw_fps))))
-            fps_t_start += elapsed
-            draw_times = []
+        self._power_f = f
+        self._peak_power = np.max(self._log_power)
 
 
-if __name__ == "__main__":
-    test_wave()
+class AnimatedWave(DataAnimation):
+    def set_samples(self, samples):
+        """
+        Show waveform, just scale samples to bounding box in x-direction, from [-1,+1] to [0, 1] in Y.
+        """
+        y_values = (-samples / 2. + 0.5) * self._bbox_height + self._bbox['top']
+        x_values = np.linspace(self._bbox['left'], self._bbox['right'] - 1, y_values.size)
+        self._coords = (self._precision_mult * np.dstack([x_values, y_values])).reshape(-1, 1, 2).astype(np.int32)
--- explore.py
+++ explore.py
@@ -20,7 +20,7 @@ from gui_utils.drawing import blend_colors, in_bbox
 from sound_tools import sound
 
 from fm_synth import FMSynthesizer
-from drawing import AnimatedWave
+from drawing import AnimatedWave, AnimatedSpectrum
 from instructions import HELP_TEXT
 
 
@@ -42,9 +42,11 @@ class FMExplorerApp(object):
     HELP_BKG = (42, 42, 42, 255)
     HELP_FONT = cv2.FONT_HERSHEY_SIMPLEX
     SAMPLING_RATE = 44100
-    HELP_OPACITY = None  # 0.9  # None for less CPU
+    HELP_OPACITY = 0.9  # None for less CPU
     TRANSLUCENT = 0.5
     TITLE_OPACITY = 0.15
+
+    WINDOW_SEPARATION = 5
 
     M_GRID_COLORS = {'heavy': MODULATION_COLOR,
                      'light': blend_colors(MODULATION_COLOR, BKG_COLOR, TRANSLUCENT).tolist(),
@@ -67,44 +69,46 @@ class FMExplorerApp(object):
                      'bkg': BKG_COLOR}
 
     H_DIV_LINE = 0.7  # control and display
-    V_DIV_LINE = 0.7  # wave and spectrum
+    V_DIV_LINE = 0.6  # wave and spectrum
 
     def __init__(self, window_size,
                  init_carrier_max=(1000.0, MAX_VOL),
                  init_modulation_max=(50.0, 100.),
                  carrier_init=(210., 0.5),
-                 modulation_init=(10, 20.)):
+                 modulation_init=(10, 0.)):
         """
         initialize app window
         :param window_size:  Width x height
         """
+        # state
+        self._showing_help = True
+        self._mouse_pos = 0, 0
+        self._last_param_set = None  # update animations when this changes
         self._app_t_start = time.perf_counter()
         self._win_size = window_size
         self._win_name = "FM Explorer"
-        self._adjusting_modulation = False
-        self._help_display = StatusMessages(window_size[::-1],
-                                            text_color=FMExplorerApp.HELP_COLOR,
-                                            bkg_color=FMExplorerApp.HELP_BKG, fullscreen=True,
-                                            font=FMExplorerApp.HELP_FONT, bkg_alpha=FMExplorerApp.HELP_OPACITY)
-
-        self._help_display.add_msgs(HELP_TEXT, 'help', 0.)
-
+        self._adjusting_modulation = False  # mode
         # init sound & synth
-        self._showing_help = True
         self._fm = FMSynthesizer(carrier_init=carrier_init, modulation_init=modulation_init)
 
-        # init gui
-        self._n_waveform_samples = int(FMExplorerApp.SAMPLING_RATE / 10.)
+        # init gui layout & colors
+        self._n_waveform_samples = int(FMExplorerApp.SAMPLING_RATE / 20.)
+        self._waveform_samples = None  # for animations, only update when FM params / window size changes
+
         self._blank = np.zeros((window_size[1], window_size[0], 4), np.uint8)
         self._blank[:, :, 3] = 255
         h_div_line = int(self._win_size[1] * FMExplorerApp.H_DIV_LINE)
         v_div_line = int(self._win_size[0] * FMExplorerApp.V_DIV_LINE)
-        self._control_bbox = {'top': 0, 'bottom': h_div_line,
-                              'left': 0, 'right': window_size[0]}
-        self._wave_bbox = {'top': h_div_line, 'bottom': window_size[1],
-                           'left': 0, 'right': v_div_line}
-        self._spectrum_bbox = {'top': h_div_line, 'bottom': window_size[1],
-                               'left': v_div_line, 'right': window_size[0]}
+        spectrum_f_range = [0.0, 1000.0]
+
+        s = FMExplorerApp.WINDOW_SEPARATION
+        self._control_bbox = {'top': s*2, 'bottom': h_div_line - s,
+                              'left': s*2, 'right': window_size[0]-s*2}
+        self._wave_bbox = {'top': h_div_line + s, 'bottom': window_size[1]-s*2,
+                           'left': s*2, 'right': v_div_line - s}
+        self._spectrum_bbox = {'top': h_div_line + s, 'bottom': window_size[1]-s*2,
+                               'left': v_div_line + s, 'right': window_size[0]-s*2}
+
         self._m_grid = CartesianGrid(self._control_bbox, init_values=modulation_init, axis_labels=('F', 'D'),
                                      draw_props={'cursor_string': "(%.2f Hz, %.2f)"},
                                      param_ranges=[[0, init_modulation_max[0]], [0, init_modulation_max[1]]],
@@ -115,41 +119,64 @@ class FMExplorerApp(object):
                                      param_ranges=[[0, init_carrier_max[0]], [0, init_carrier_max[1]]],
                                      colors=FMExplorerApp.C_GRID_COLORS,
                                      title='carrier', adjustability=(True, False))
-        self._s_grid = CartesianGrid(self._spectrum_bbox, init_values=(None, None), axis_labels=('F', ' '),
-                                     draw_props={'cursor_string': "(%.2f Hz, power = %.2f)",
-                                                 'title_font_scale': 1., 'cursor_font_scale': .4,
-                                                 'title_thickness': 1},
-                                     param_ranges=[[0., 1.], [0., 1.]],
+        self._s_grid = CartesianGrid(self._spectrum_bbox, init_values=(None, None), axis_labels=('F (Hz)', 'log(p)'),
+                                     draw_props={'cursor_string': None,
+                                                 'title_font_scale': 1., 'cursor_font_scale': .4, 'axis_font_scale': .4,
+                                                 'title_thickness': 1, 'show_ticks': (True, False)},
+                                     param_ranges=[spectrum_f_range, [0., 1.]],
                                      colors=FMExplorerApp.S_GRID_COLORS,
-                                     title='power spectrum', adjustability=(False, False))
-        self._w_grid = CartesianGrid(self._wave_bbox, init_values=(None, None), axis_labels=('F', None),
+                                     title='power spectrum', adjustability=(True, False))
+
+        self._w_grid = CartesianGrid(self._wave_bbox, init_values=(None, None), axis_labels=('T', None),
                                      draw_props={'cursor_string': None, 'title_font_scale': 1.5,
-                                                 'title_thickness': 1,'show_ticks':(True, False)},
-                                     param_ranges=[[0., self._n_waveform_samples/FMExplorerApp.SAMPLING_RATE]
+                                                 'title_thickness': 1, 'show_ticks': (True, False)},
+                                     param_ranges=[[0., self._n_waveform_samples / FMExplorerApp.SAMPLING_RATE]
                                          , [-.2, 1.2]],
                                      colors=FMExplorerApp.W_GRID_COLORS,
                                      title='waveform', adjustability=(False, False))
 
-        self._wave = AnimatedWave(self._wave_bbox, wave_func=self._make_wave_samples,
-                                  bbox_thickness=2,
-                                  wave_color=FMExplorerApp.WAVE_COLOR)
-        self._mouse_pos = 0, 0
+        # help
+        self._help_display = StatusMessages(window_size[::-1],
+                                            text_color=FMExplorerApp.HELP_COLOR,
+                                            bkg_color=FMExplorerApp.HELP_BKG, fullscreen=True,
+                                            font=FMExplorerApp.HELP_FONT, bkg_alpha=FMExplorerApp.HELP_OPACITY)
+        self._help_display.add_msgs(HELP_TEXT, 'help', 0.)
+
+        # init animations
+        self._spectrum = AnimatedSpectrum(self._spectrum_bbox, f_range=spectrum_f_range,
+                                          color=FMExplorerApp.SPECTRUM_COLOR)
+        self._wave = AnimatedWave(self._wave_bbox, color=FMExplorerApp.WAVE_COLOR)
+        self._set_animation_samples()
+
         self._run()
 
-    def _make_wave_samples(self):
+    def _need_to_update_animations(self):
+        param_set = tuple(self._c_grid.get_values().tolist() + self._m_grid.get_values().tolist())
+        if self._last_param_set is None or self._last_param_set != param_set:
+            self._last_param_set = param_set
+            return True
+        return False
 
-        return self._fm.get_samples(self._n_waveform_samples)[0]
+    def _set_animation_samples(self):
+        samples = self._fm.get_samples(self._n_waveform_samples)[0]
+        self._wave.set_samples(samples)
+
+        self._spectrum.set_samples(samples)
 
     def _mouse(self, event, x, y, flags, param):
         self._mouse_pos = x, y
-        # update grid
+
+        # update grids
         if self._adjusting_modulation:
             self._m_grid.mouse(event, x, y, flags, param)
         else:
             self._c_grid.mouse(event, x, y, flags, param)
-
         self._s_grid.mouse(event, x, y, flags, param)
         self._w_grid.mouse(event, x, y, flags, param)
+
+        # update animations if params changed
+        if self._need_to_update_animations():
+            self._set_animation_samples()
 
         # update synth:
         if event == cv2.EVENT_MOUSEMOVE:
@@ -172,15 +199,18 @@ class FMExplorerApp(object):
     def _make_frame(self):
         frame = self._blank.copy()
 
+        # add grids to frame
         if self._adjusting_modulation:
             self._m_grid.draw(frame)
         else:
             self._c_grid.draw(frame)
-
         self._s_grid.draw(frame)
         self._w_grid.draw(frame)
 
+        # add animations
         self._wave.draw(frame)
+        self._spectrum.draw(frame)
+
         if self._showing_help:
             # import ipdb; ipdb.set_trace()
             n_chan = 3 if FMExplorerApp.HELP_OPACITY is None else 4
@@ -209,7 +239,7 @@ class FMExplorerApp(object):
                 n_frames = 0
                 t_start = time.perf_counter()
 
-    def _keyboard(self,k):
+    def _keyboard(self, k):
 
         # send keystroke to appropriate grid
         if in_bbox(self._control_bbox, self._mouse_pos):
@@ -224,23 +254,32 @@ class FMExplorerApp(object):
                 self._fm.set_params(c_freq=c_freq, c_amp=c_amp, m_freq=None, m_depth=None)
 
         elif in_bbox(self._wave_bbox, self._mouse_pos):
-            _ = self._w_grid.keyboard(k)
+            # _ = self._w_grid.keyboard(k)  nothing for wave to do
+            pass
 
         elif in_bbox(self._spectrum_bbox, self._mouse_pos):
-            new_spectrum_ranges = self._s_grid.keyboard(k)
+            new_param_range = self._s_grid.keyboard(k)  # might change here, update animation
+            if new_param_range is not None:
+                self._spectrum.set_f_range(new_param_range[0, :], new_param_range[1, :])
 
         if k & 0xff == ord('q'):
             return True
         if k & 0xff == ord('h'):
             self._showing_help = not self._showing_help
+
         elif k & 0xff == ord(' '):
             self._adjusting_modulation = not self._adjusting_modulation
+
+        # changing number of samples in wave, update grids and animations
         elif k & 0xff == ord(','):
             self._n_waveform_samples = int(self._n_waveform_samples * 0.75)
-            self._w_grid.set_param_range(self._n_waveform_samples/FMExplorerApp.SAMPLING_RATE, 0)
+            self._w_grid.set_param_range(self._n_waveform_samples / FMExplorerApp.SAMPLING_RATE, 0)
+            self._set_animation_samples()
         elif k & 0xff == ord('.'):
             self._n_waveform_samples = int(self._n_waveform_samples * 1.25)
-            self._w_grid.set_param_range(self._n_waveform_samples/FMExplorerApp.SAMPLING_RATE, 0)
+            self._w_grid.set_param_range(self._n_waveform_samples / FMExplorerApp.SAMPLING_RATE, 0)
+            self._set_animation_samples()
+
         return False
 
 
