844d84ce26f2f846d132503b69ce3f5e033d6d79
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2023-01-31 22:02:32 -0800

    static parts

--- staff.py
+++ staff.py
@@ -1,92 +1,202 @@
 """
-Layout of items on staff that don't move.
+Basic musical typesetting.
 """
+import logging
+
 from clef_drawing import make_bass, make_treble
-from music_layout import LAYOUT
+from music_layout import LAYOUT, DEFAULT_COLORS_BGRA
 import numpy as np
 import cv2
 
 
-class Shape(object):
-    PRECISION_BITS = 6
-
-    def __init__(self):
-        self._parts = []
-
-    def add_polyline(self, coords, color, thickness, closed, filled):
-        coords = (coords * (2. ** Shape.PRECISION_BITS)).astype(np.int32)
-        self._parts.append((coords, color, thickness, closed, filled))
-        return self
-
-    def add_box(self, x_span, y_span, color, thickness):
-        box_points = np.array([[x_span[0], y_span[0]],
-                               [x_span[1], y_span[0]],
-                               [x_span[1], y_span[1]],
-                               [x_span[0], y_span[1]]])
-        return self.add_polyline(box_points, color, thickness=thickness, closed=True, filled=False)
-
-    def draw(self, img):
-        for coords, color, thickness, closed, filled in self._parts:
-            if filled:
-                cv2.fillPoly(img, [coords], color, lineType=cv2.LINE_AA, shift=Shape.PRECISION_BITS)
-            else:
-                cv2.polylines(img, [coords], closed, color, thickness=thickness, lineType=cv2.LINE_AA,
-                              shift=Shape.PRECISION_BITS)
-
-
 class Staff(object):
+    """
+    Draw treble & bass staves & clefs.
+    Draw crescendo wedge under.
+    """
+
+    PRECISION_BITS = 6  # drawing
+
     def __init__(self, bbox, colors=None):
         self._bbox = bbox
-        self._width, self._height = self._bbox['right'] - self._bbox['left'], self._bbox['bottom'] - self._bbox['top']
+        self._colors = colors if colors is not None else DEFAULT_COLORS_BGRA
+        self._width = self._bbox['right'] - self._bbox['left']
+        self._height = self._bbox['bottom'] - self._bbox['top']
 
-        # staff area of image
-        self._top_y = int(self._bbox['top'] + self._height * LAYOUT['v_span'][0])
-        bottom_y = int(self._bbox['top'] + self._height * LAYOUT['v_span'][1])
-        self._left_x = int(self._bbox['left'] + self._width * LAYOUT['h_span'][0])
-        self._right_x = int(self._bbox['left'] + self._width * LAYOUT['h_span'][1])
+        self._calc_staff_dims()
+        font_scale = 1 + int(self._space / 40)
 
-        self._width = self._right_x - self._left_x
-        height = bottom_y - self._top_y
-        self._space = int(height / 10)  # space between ledger lines
-        # re-define to avoid aliasing of staff lines
-        self._height = self._space * 10
-        self._bottom_y = self._top_y + self._height
+        # save these for fast drawing
+        self._normal_line_key = (self._colors['lines'], max(1, int(self._staff_line_thickness - 1)), False)
+        self._lines = {
+            self._normal_line_key: []}  # key is (color, thickness, closed)-tuple (2 below), value is list of arrays of points to join
 
-        self._middle_c_y = ((self._top_y + self._top_y + bottom_y) / 2)
+        self._rects = []  # list of dicts {'coords':, 'color':}
 
-        # thickness of lines
-        min_thickness = self._space / 10
-        self._line_thickness = int(min_thickness)
-        self._note_stem_thickness = int(min_thickness * 1.5)
+        self._set_staff_lines()
+        self._set_clef_lines()
+        self._set_wedge_lines()
+        self._set_note_lines()
+        self._set_text_positions()
 
+        # convert all coordinates
+        precision_mult = 2. ** Staff.PRECISION_BITS
+        for key in self._lines:
+            self._lines[key] = [np.int32(coords * precision_mult) for coords in self._lines[key]]
+
+    def _set_text_positions(self):
+        self._title_thickness = max(1, int(self._staff_line_thickness / 2))
+        self._dynamics_thickness = max(1, self._title_thickness-1)
+        self._title_font_scale = (self._space / 12)
+        self._dynamics_font_scale = (self._space / 21)
+        (w, h), _ = cv2.getTextSize(LAYOUT['title']['txt'], LAYOUT['title']['font'], self._title_font_scale,
+                                    self._title_thickness)
+        x = int((self._width - w) / 2)
+        y = int(self._bbox['top'] + self._space + h)
+        self._title_pos = x, y
+
+        (ppp_w, ppp_h), _ = cv2.getTextSize('ppp', LAYOUT['dynamics']['font'], self._dynamics_font_scale, self._dynamics_thickness)
+        wedge_to_text_space = max(1, int(self._space / 1.5))
+        self._ppp_pos = (self._wedge_left[0] - ppp_w - wedge_to_text_space, self._wedge_right[1])
+        self._fff_pos = (self._wedge_right[0] + wedge_to_text_space + 3, self._wedge_right[1])
+
+    def _set_wedge_lines(self):
+        # crescendo wedge
+        center_x = (self._staff_bbox['left'] + self._staff_bbox['right']) / 2
+        wedge_left_x = int(center_x - self._width * LAYOUT['wedge_length'] / 2)
+        wedge_right_x = int(center_x + self._width * LAYOUT['wedge_length'] / 2)
+        wedge_y_center = self._staff_bbox['bottom'] + int(self._space * 4)
+        wedge_right_ys = wedge_y_center - self._space * .5, \
+                         wedge_y_center + self._space * .5
+        self._wedge_left = wedge_left_x, wedge_y_center
+        self._wedge_right = wedge_right_x, wedge_y_center
+        wedge_right_up = wedge_right_x, wedge_right_ys[0]
+        wedge_right_down = wedge_right_x, wedge_right_ys[1]
+        self._lines[self._normal_line_key].append(np.array([wedge_right_down, self._wedge_left, wedge_right_up]))
+
+    def _calc_staff_dims(self):
+        staff_bbox_top = int(self._bbox['top'] + self._height * LAYOUT['staff_v_span'][0])
+        staff_bbox_bottom = int(self._bbox['top'] + self._height * LAYOUT['staff_v_span'][1])
+        staff_bbox_left = int(self._bbox['left'] + self._width * LAYOUT['staff_h_span'][0])
+        staff_bbox_right = int(self._bbox['left'] + self._width * LAYOUT['staff_h_span'][1])
+
+        # re-define height to avoid aliasing of staff lines
+        self._staff_width = staff_bbox_right - staff_bbox_left
+        staff_height = staff_bbox_bottom - staff_bbox_top
+        self._space = int(staff_height / 10)  # space between ledger lines
+        staff_height = self._space * 10
+        staff_bbox_bottom = staff_bbox_top + staff_height
+        self._staff_bbox = {'top': staff_bbox_top,
+                            'bottom': staff_bbox_bottom,
+                            'left': staff_bbox_left,
+                            'right': staff_bbox_right}
+
+        self._staff_line_thickness = int(self._space / 10)
+        self._note_stem_thickness = int(self._staff_line_thickness * 1.5)
+
+        self._middle_c_y = ((self._staff_bbox['top'] + self._staff_bbox['bottom']) / 2)  # should make integer?
+
+    def _set_staff_lines(self):
+        #  staff lines, vertical, single & double thickness lines on left
+        # are rects to keep from looking sloppy
+
+        # thick vert. line
+        self._rects = [{'coords': [[self._staff_bbox['top'],
+                                    self._staff_bbox['bottom']],
+                                   [self._staff_bbox['left'],
+                                    self._staff_bbox['left'] + self._staff_line_thickness * 2]],
+                        'color': self._colors['lines']}]
+        # thin vert. line
+        self._rects.append({'coords': [[self._staff_bbox['top'],
+                                        self._staff_bbox['bottom']],
+                                       [self._staff_bbox['left'] + self._staff_line_thickness * 3,
+                                        self._staff_bbox['left'] + self._staff_line_thickness * 4]],
+                            'color': self._colors['lines']})
+
+        # the ten staff lines
+        h_line_y = np.linspace(self._staff_bbox['top'], self._staff_bbox['bottom'], 11).astype(np.int64).tolist()
+        line_ys = np.array(h_line_y[:5] + h_line_y[6:])
+        self._rects.extend([{'coords': [[y, y + self._staff_line_thickness],
+                                        [self._staff_bbox['left'], self._staff_bbox['right']]],
+                             'color': self._colors['lines']} for y in line_ys])
+
+    def _set_clef_lines(self):
         # Clef coordinates
         treble_y = self._middle_c_y - self._space * 2.0  # G
         bass_y = self._middle_c_y + self._space * 2.0  # F
-        clef_x = self._left_x + LAYOUT['clef_indent_spaces'] * self._space
+        clef_x = self._staff_bbox['left'] + LAYOUT['clef_indent_spaces'] * self._space
         treble_pos = np.array((clef_x, treble_y)).reshape(1, -1)
         bass_pos = np.array((clef_x, bass_y)).reshape(1, -1)
-        self._bass_clef_coords = [(coords * self._space + bass_pos, is_closed)
-                                  for coords, is_closed in zip(*make_bass())]
-        self._treble_clef_coords = [(coords * self._space + treble_pos, is_closed)
-                                    for coords, is_closed in zip(*make_treble())]
+        bass = make_bass()
+        treble = make_treble()
 
-        #  staff lines, vertical, single & double thickness lines on left
-        self._light_v_line = np.array([[self._left_x + self._space * 4, self._top_y],
-                                       [self._left_x + self._space * 4, self._bottom_y]])
-        self._heavy_v_line = np.array([[self._left_x, self._top_y],
-                                       [self._left_x, self._bottom_y]])
-        # horizontal
-        h_line_y = np.linspace(self._top_y, bottom_y, 11).astype(np.int64).tolist()
-        line_ys = np.array(h_line_y[:5] + h_line_y[6:])
-        self._h_lines = [np.array([[self._left_x, y],
-                                   [self._right_x, y]]) for y in line_ys]
-        # font = cv2.FONT_HERSHEY_DUPLEX
-        # font_scale = 1 + int(spacing / 40)
+        self._lines[self._normal_line_key].extend([coords * self._space + bass_pos
+                                                   for coords, closed in zip(*bass)])
+
+        self._lines[self._normal_line_key].extend([coords * self._space + treble_pos
+                                                   for coords, closed in zip(*treble)])
+
+    def _set_note_lines(self):
+        pass
+
+    def draw(self, frame, show_box=False):
+        frame[self._bbox['top']:self._bbox['bottom'], self._bbox['left']: self._bbox['right'], :] = self._colors['bkg']
+
+        # rects
+        for rect in self._rects:
+            ((i0, i1), (j0, j1)) = rect['coords']
+
+            frame[i0:i1, j0:j1, :] = rect['color']
+
+        # lines
+        for line_info in self._lines:
+            color, thickness, closed = line_info
+            cv2.polylines(frame, self._lines[line_info], closed, color, thickness=int(thickness), lineType=cv2.LINE_AA,
+                          shift=Staff.PRECISION_BITS)
+
+        # text
+
+        cv2.putText(frame, LAYOUT['title']['txt'], self._title_pos, LAYOUT['title']['font'],
+                    self._title_font_scale, self._colors['text'], self._title_thickness, cv2.LINE_AA)
+
+        cv2.putText(frame, "ppp", self._ppp_pos, LAYOUT['dynamics']['font'],
+                    self._dynamics_font_scale, self._colors['text'], self._title_thickness, cv2.LINE_AA)
+        cv2.putText(frame, "fff", self._fff_pos, LAYOUT['dynamics']['font'],
+                    self._dynamics_font_scale, self._colors['text'], self._dynamics_thickness, cv2.LINE_AA)
 
 
-class Note(object):
+def test_staff_drawing():
+    window_size = (590, 600)
+    s = Staff({'top': 50, 'bottom': window_size[1] - 10, 'left': 10, 'right': window_size[0] - 10})
+    blank = np.zeros((window_size[1], window_size[0], 4), dtype=np.uint8)
+    win_name = "Staff test"
+    cv2.namedWindow(win_name, cv2.WINDOW_AUTOSIZE)
+    # cv2.setMouseCallback(win_name, s.mouse)
+    frame = blank.copy()
+    
+    s.draw(frame, show_box=False)
+    cv2.imwrite('test.jpg', frame)
+    return
 
-    def __init__(self, bbox, colors):
+    n_frames = 0
+    t_start = time.perf_counter()
+    while True:
+        frame = blank.copy()
+        s.draw(frame, show_guidebox=False)
+        s.draw_note(frame)
 
-        self._bbox = bbox
-        self._width, self._height = self._bbox['right'] - self._bbox['left'], self._bbox['bottom'] - self._bbox['top']
+        cv2.imshow(win_name, frame)
+        k = cv2.waitKey(1)
+        if k & 0xff == ord('q'):
+            break
+        n_frames += 1
+        now = time.perf_counter()
+        if now - t_start > 2:
+            print("FPS:  %.3f" % (n_frames / (now - t_start),))
+            t_start = now
+            n_frames += 1
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    test_staff_drawing()
